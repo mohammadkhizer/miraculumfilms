6b7c114d78c5f11cf687129a44d47480959d345d
mohammadkhizer <mohmmadekhizers@gmail.com>
2025-05-15 18:54:31 +0530

    Host

---
+++ _next/static/chunks/app/layout-6b93e8de961e4cbd.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9336:function(e,r,t){Promise.resolve().then(t.bind(t,3999)),Promise.resolve().then(t.t.bind(t,10,23)),Promise.resolve().then(t.t.bind(t,8877,23))},3999:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return s}});var n=t(7437),o=t(2265);function s(e){let{children:r}=e;return(0,o.useEffect)(()=>{let e=new IntersectionObserver(e=>{e.forEach(e=>{e.isIntersecting&&e.target.classList.add("in-view")})},{root:null,rootMargin:"0px",threshold:.1}),r=document.querySelectorAll(".animate-fade-in");return r.forEach(r=>e.observe(r)),()=>{r.forEach(r=>e.unobserve(r))}},[]),(0,n.jsx)(n.Fragment,{children:r})}},8877:function(){},10:function(e){e.exports={style:{fontFamily:"'__Montserrat_4bc053', '__Montserrat_Fallback_4bc053'",fontStyle:"normal"},className:"__className_4bc053",variable:"__variable_4bc053"}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),a=Object.prototype.hasOwnProperty,c=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function l(e,r,t){var n,s={},l=null,f=null;for(n in void 0!==t&&(l=""+t),void 0!==r.key&&(l=""+r.key),void 0!==r.ref&&(f=r.ref),r)a.call(r,n)&&!i.hasOwnProperty(n)&&(s[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===s[n]&&(s[n]=r[n]);return{$$typeof:o,type:e,key:l,ref:f,props:s,_owner:c.current}}r.Fragment=s,r.jsx=l,r.jsxs=l},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,864,744],function(){return e(e.s=9336)}),_N_E=e.O()}]);
